a7a199e48f2ed3597390819fa1ebcbd09951e632
justinr <justin@worldcrafters.io>
2023-03-16 01:16:05 -0700

    Program can successfully remove apps not in config

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use std::env;
 use std::fs;
 use std::process::Command;
+// use libflatpak::prelude::*;
 
 // NOTE: DO NOT RUN THIS WITH shell.nix, just use regular 'cargo run'
 
@@ -12,8 +13,10 @@ struct Application {
     installation: String,
     remote: String,
     appid: String,
+    local: Option<bool>,
 }
 
+#[derive(Debug)]
 struct Remote {
     name: String,
     options: String,
@@ -23,17 +26,18 @@ fn main() {
 
     let config_entries: Vec<String> = retrieve_config_entries();
     let list_config: Vec<Application> = genlist_config(&config_entries);
-    let list_system: Vec<Application> = genlist_system();
-    let list_install: Vec<Application> = genlist_install(&list_system, &list_config);
-    let list_remove: Vec<Application> = genlist_remove(&list_system, &list_config);
+    let mut list_system: Vec<Application> = genlist_system();
 
-    install_apps(&list_install);
-    remove_apps(&list_remove);
+    check_system(&list_config, &mut list_system);
+    remove_apps(&list_system);
 
-    println!("*** list_config ***");
-    for apps in list_config {
-        println!("install: {}, remote: {}, appid: {}\n", apps.installation, apps.remote, apps.appid);
-    }
+    // check_config(&list_config, &mut list_system);
+    install_apps(&list_config, &list_system);
+
+    // println!("*** list_config ***");
+    // for apps in list_config {
+    //     println!("install: {}, remote: {}, appid: {}\n", apps.installation, apps.remote, apps.appid);
+    // }
     println!("*** list_system ***");
     for apps in list_system {
         println!("install: {}, remote: {}, appid: {}\n", apps.installation, apps.remote, apps.appid);
@@ -51,11 +55,13 @@ fn main() {
 }
 
 fn retrieve_config_entries() -> Vec<String> {
+    // BUG: newline is being inserted before system!
     let username = env::var("USER").expect("failed to get username");
     let config_path = format!("/home/{}/.config/flatpak-declare/config", &username);
     let config_contents = fs::read_to_string(&config_path).expect("could not read contents of config file");
     let config_entries: Vec<String> = config_contents
         .split(',')
+        .map(|field| field.trim())
         .map(|entry| entry.into()) // transform each &str element in the iterator produced by config_contents.split(',') into a String. This avoids referencing the local variable 'config_contents' since it will be dropped when function scope ends.
         .collect();
     config_entries
@@ -70,6 +76,7 @@ fn genlist_config(config_entries: &Vec<String>) -> Vec<Application> {
                 installation: fields[0].to_string(),
                 remote: fields[1].to_string(),
                 appid: fields[2].to_string(),
+                local: None,
             })
         }
         // else {
@@ -96,37 +103,115 @@ fn genlist_system() -> Vec<Application> {
             list_system.push(Application {
                 installation: fields[0].to_string(),
                 remote: fields[1].to_string(),
-                appid: fields[2].to_string()
+                appid: fields[2].to_string(),
+                local: None,
             })
         }
     }
     list_system
 }
 
-fn genlist_install(list_system: &Vec<Application>,
-                   list_config: &Vec<Application>,) -> Vec<Application> {
-    let list_install: Vec<Application> = Vec::new();
-    // TODO: Generate list_install vector
-    // for every struct in list_config
+// fn genlist_install(list_system: &Vec<Application>,
+//                    list_config: &Vec<Application>,) -> Vec<Application> {
+//     let list_install: Vec<Application> = Vec::new();
+//     // TODO: Generate list_install vector
+//     // for every struct in list_config
+//         // if (remote AND appid AND installation) from list_config is NOT present in list_system
+//             // add relevant struct to list_install
+//     list_install
+// }
+
+// fn genlist_remove(list_system: &Vec<Application>,
+//                   list_config: &Vec<Application>,) -> Vec<Application> {
+//     let list_remove: Vec<Application> = Vec::new();
+//     // your code here!
+//     list_remove
+// }
+
+fn check_system(list_config: &Vec<Application>, list_system: &mut Vec<Application>){
+    for app_sys in list_system {
+        for app_config in list_config {
+            if app_sys.installation == app_config.installation && app_sys.remote == app_config.remote && app_sys.appid == app_config.appid {
+                // println!("The system app {:?} is present in the config file.", app_sys);
+                app_sys.local = Some(true);
+                // BUG: This is setting ALL of the structs' local fields to true
+            }
+        }
+    }
+}
+
+fn remove_apps(list_system: &Vec<Application>){
+    for app_sys in list_system {
+        println!("{:?}",app_sys);
+        if app_sys.local != Some(true){
+            // remove the app [ flatpak --system uninstall --noninteractive org.gnucash.GnuCash ]
+            // [ flatpak <installation flag> uninstall --noninteractive <appid/ref> ]
+            println!("{} is not not present in the config file!",app_sys.appid);
+            let install_flag = format!("--{}", app_sys.installation);
+            let app_id = format!("{}", app_sys.appid);
+            let remove = Command::new("flatpak")
+                .arg(install_flag)
+                .arg("uninstall")
+                .arg("--noninteractive")
+                .arg(app_id)
+                .output()
+                .expect("failed to execute process");
+            if remove.status.success() {
+                println!("Removed {} successfully", app_sys.appid);
+            } else {
+                eprintln!("Failed to remove package: {}", String::from_utf8_lossy(&remove.stderr));
+            }
+        }
+    }
+
+    // flatpak uninstall --unused (noninteractive)
+    // let remove_unused = Command::new("flatpak")
+    //     .arg("uninstall")
+    //     .arg("--unused")
+    //     .arg("--noninteractive")
+    //     .output()
+    //     .expect("failed to execute process");
+    // if remove_unused.status.success() {
+    //     println!("Removed unused packages successfully");
+    // } else {
+    //     eprintln!("Failed to remove unused packages: {}", String::from_utf8_lossy(&remove_unused.stderr));
+    // }
+
+    // flatpak uninstall --delete-data (NOTE: Make this interactive!)
+    // let remove_unused = Command::new("flatpak")
+    //     .arg("uninstall")
+    //     .arg("--delete-data")
+    //     .output()
+    //     .expect("failed to execute process");
+    // if remove_unused.status.success() {
+    //     println!("Deleted orphaned app-data successfully");
+    // } else {
+    //     eprintln!("Failed to remove unused packages: {}", String::from_utf8_lossy(&remove_unused.stderr));
+    // }
+}
+
+fn install_apps(list_config: &Vec<Application>, list_system: &Vec<Application>){
+    // for every struct in list_config:
         // if (remote AND appid AND installation) from list_config is NOT present in list_system
-            // add relevant struct to list_install
-    list_install
-}
+            // set 'local' field from list_config copy to true
 
-fn genlist_remove(list_system: &Vec<Application>,
-                  list_config: &Vec<Application>,) -> Vec<Application> {
-    let list_remove: Vec<Application> = Vec::new();
-    // your code here!
-    list_remove
-}
+    // for every struct in list_config:
+        // if local field is true
+            // install the app [ flatpak --system install --noninteractive flathub org.gnucash.GnuCash ]
+                // [ flatpak <installation flag> install --noninteractive <remote> <appid/ref> ]
 
-fn install_apps(list_install: &Vec<Application>){
-    // iterate through remotes
-        // run 'flatpak --system install flathub org.qutebrowser.qutebrowser'
-        // run 'flatpak <install flag> install <remote> <appid>'
-}
-
-fn remove_apps(list_remove: &Vec<Application>){
-    // if applications vector contains struct with given appid AND remote, remove the app with the given appid AND remote
-    // uninstall unused apps
+    // let package_name = "com.example.MyApp";
+    // let repository_name = "flathub";
+    // let output = Command::new("flatpak")
+    //     .arg("install")
+    //     .arg("-y") // Assume 'yes' to prompts (optional, useful for non-interactive environments)
+    //     .arg(repository_name)
+    //     .arg(package_name)
+    //     .output()
+    //     .expect("Failed to execute command");
+    // if output.status.success() {
+    //     println!("Installed package successfully");
+    // } else {
+    //     eprintln!("Failed to install package: {}", String::from_utf8_lossy(&output.stderr));
+    // }
 }
