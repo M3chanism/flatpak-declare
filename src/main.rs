134862dc8bf976bd34e058ee5f767f0ed32f612b
justinr <justin@worldcrafters.io>
2023-03-15 19:03:29 -0700

    Made list generator functions self contained.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use std::env;
 use std::fs;
 use std::process::Command;
@@ -18,23 +21,26 @@ struct Remote {
 
 fn main() {
 
+    // TODO: Move config entry retrieval into 'retrieve_config_entries' function
     let username = env::var("USER").expect("failed to get username");
     let config_path = format!("/home/{}/.config/flatpak-declare/config", &username);
     let config_contents = fs::read_to_string(&config_path).expect("could not read contents of config file");
-    let lines: Vec<&str> = config_contents.split(',').collect();
-    let mut list_config: Vec<Application> = Vec::new();
-    let mut list_system: Vec<Application> = Vec::new();
-    let mut list_install: Vec<Application> = Vec::new();
-    let mut list_remove: Vec<Application> = Vec::new();
+    let config_entries: Vec<&str> = config_contents.split(',').collect();
 
-    genlist_config(&lines, &mut list_config);
-    genlist_system(&mut list_system);
-    genlist_install(&list_system, &list_config, &mut list_install);
-    genlist_remove(&list_system, &list_config, &mut list_remove);
+    let list_config: Vec<Application> = genlist_config(&config_entries);
+    let list_system: Vec<Application> = genlist_system();
+    let list_install: Vec<Application> = genlist_install(&list_system, &list_config);
+    let list_remove: Vec<Application> = genlist_remove(&list_system, &list_config);
+
     install_apps(&list_install);
     remove_apps(&list_remove);
 
-    for apps in list_config {
+    // println!("*** list_config ***");
+    // for apps in list_config {
+    //     println!("install: {}, remote: {}, appid: {}\n", apps.installation, apps.remote, apps.appid);
+    // }
+    println!("*** list_system ***");
+    for apps in list_system {
         println!("install: {}, remote: {}, appid: {}\n", apps.installation, apps.remote, apps.appid);
     }
 
@@ -49,9 +55,9 @@ fn main() {
     // println!("{}", String::from_utf8_lossy(&remotes.stdout));
 }
 
-fn genlist_config(lines: &Vec<&str>, list_config: &mut Vec<Application>){
-    for line in lines {
-        // push_fields(line, list_config);
+fn genlist_config(config_entries: &Vec<&str>) -> Vec<Application> {
+    let mut list_config = Vec::new();
+    for line in config_entries {
         let fields: Vec<&str> = line.split("::").collect();
         if fields.len() >= 3 {
             list_config.push(Application {
@@ -64,33 +70,41 @@ fn genlist_config(lines: &Vec<&str>, list_config: &mut Vec<Application>){
         //     println!("Error: line does not contain enough fields");
         // }
     }
+    list_config
 }
 
-fn genlist_system(list_system: &mut Vec<Application>){
+fn genlist_system() -> Vec<Application> {
+    let list_system: Vec<Application> = Vec::new();
     let apps = Command::new("flatpak")
         .arg("list")
         .arg("--app")
-        .arg("--columns=application")
+        .arg("--columns=installation,origin,application")
         .output()
         .expect("failed to execute 'flatpak list --app --columns=application'");
+    println!("*** from inside genlist_system ***");
     println!("{}", String::from_utf8_lossy(&apps.stdout));
     // TODO: parse apps string into apps_system vector
-
+    // separate entries by line
+        // separate fields by whitespace
+    list_system
 }
 
 fn genlist_install(list_system: &Vec<Application>,
-                   list_config: &Vec<Application>,
-                   mut list_install: &mut Vec<Application>){
+                   list_config: &Vec<Application>,) -> Vec<Application> {
+    let list_install: Vec<Application> = Vec::new();
     // for every struct in the app vec
-    for app in list_install{
+    for app in &list_install{
         // if remote and appid from list_config is NOT present on system (hint: use 'flatpak list')
             // add appid to list_install
     }
+    list_install
 }
 
 fn genlist_remove(list_system: &Vec<Application>,
-                  list_config: &Vec<Application>,
-                  mut list_remove: &mut Vec<Application>){
+                  list_config: &Vec<Application>,) -> Vec<Application> {
+    let list_remove: Vec<Application> = Vec::new();
+    // your code here!
+    list_remove
 }
 
 fn install_apps(list_install: &Vec<Application>){
